784ab39f9bfd970a2445eff85c61439d85afe401
Nahabi Wandera <nahabi@tgfoundry.com>
2022-12-05 23:16:15 +0300

    DEV: Warning popup

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -32,6 +32,8 @@ const LayerSelector = ({ label, value }) => (
 const Map = () => {
   const [modalVisible, setModalVisible] = useState(false)
   const [layersVisible, setLayersVisible] = useState(false)
+  const [warningVisible, setWarningVisible] = useState(true)
+
   const visibilityClass = layersVisible ? 'visible' : 'hidden'
 
   return (
@@ -165,6 +167,42 @@ const Map = () => {
             Cancel
           </Button>
         </Modal.Footer>
+      </Modal>
+
+      {/*Multiple layers warning modal*/}
+      <Modal
+        show={warningVisible}
+        size="md"
+        popup={true}
+        onClose={() => setWarningVisible(false)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <svg
+              className="w-16 h-16 text-red-600 mx-auto mb-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              ></path>
+            </svg>
+            <h3 className="mb-5 font-normal text-gray-500 dark:text-gray-400">
+              You cannot enter 'Edit' mode with multiple layers selected
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={() => setWarningVisible(false)}>
+                Close
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
       </Modal>
     </div>
   )
